3cf48deef3829de2817cd569ff150246a2bf5fc7
Tommy418 <itszdwelrg4@gmail.com>
2023-04-11 11:23:32 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes ,Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Error from "./Pages/Error";
+import Dashboard from "./Pages/Dashboard";
+import ShareLayout from "./components/ShareLayout";
+import "./App.css"
+import SingleProduct from "./Pages/SingleProduct";
+import Products from "./Pages/Products";
+import Login from "./Pages/Login";
+import { useState } from "react";
+import ProtectRoute from "./components/ProtectRoute";
+
+
+
+
+
 
 function App() {
+ const [user, setUser]= useState(null)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <h3>React Router DOM</h3>
+ 
+        
+        <Routes>
+          <Route path="/" element={<ShareLayout/>} >
+              <Route index element={<Home />} /> 
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/products/:productId" element={<SingleProduct />} />
+              <Route path="/login" element={<Login setUser={setUser} />} />
+            </Route>
+          <Route path="/dashboard" element={
+          <ProtectRoute user={user}>
+            <Dashboard user={user} />
+          </ProtectRoute>
+          } >
+             <Route path="user" element ={ <h2>User </h2>} />
+             <Route path="profile" element ={ <h2>Profile </h2>} />
+          </Route>
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
